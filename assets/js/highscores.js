39292e34e6a9253b984f1c3a09aff655a1aabd67
Chris <montyking20@gmail.com>
2022-01-23 14:17:02 -0700

    updated quiz

---
+++ assets/js/highscores.js
@@ -0,0 +1,31 @@
+// Assign const / globals 
+const scoresSection = document.querySelector('#high-scores')
+const olElem = document.createElement("ol")
+let sortedData;
+
+// Adds scores to list in doc
+const addScores = async()=> {
+    scoresSection.append(olElem)
+    sortedData.forEach(element => {
+        let userScore = `${element[0]} --- ${element[1]}`
+        let liElem = document.createElement("li")
+        olElem.append(liElem)
+        liElem.appendChild(document.createTextNode(userScore)) 
+    });
+}
+
+// Check if high score exists if so sort data 
+if(localStorage.scores) {
+    const data = JSON.parse(localStorage.scores)
+    sortedData = data.sort((a, b) => b[1] - a[1])
+    addScores()
+}
+
+// Func to clear scores and refresh page
+const clearScore = async ()=> {
+    localStorage.removeItem('scores')
+    location.reload()
+}
+
+// Event listener
+document.querySelector('#clear-scores').addEventListener('click', clearScore)
